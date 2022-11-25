25580a08814a39660d1166c66b25322a8da46e41
GabrielKooseng <114022797+GabrielKooseng@users.noreply.github.com>
2022-11-25 09:33:14 -0800

    fixing the hours

--- Assets/Scripts/DayTimeController.cs
+++ Assets/Scripts/DayTimeController.cs
@@ -18,6 +18,11 @@ public class DayTimeController : MonoBehaviour
     {
         get { return time / 3600f; }//con esto cambio el contador de segundo a que muestre horas
     }
+
+    float Minutes
+    {
+        get { return time % 3600f / 60f; }//cambiamos los molestos decimales del contador a solo 2
+    } 
     
     //----------------------------Esto es para oscurecer un poco las cosas-------------------------------------
     [SerializeField] Color nightLightColor;
@@ -27,8 +32,13 @@ public class DayTimeController : MonoBehaviour
 
     private void Update()
     {
+        int mm = (int)Minutes;
+        int hh = (int)Hours;
+
         time += Time.deltaTime * timeScale;//con esto regulo el tiempo para que sea mas rapido
-        text.text = Hours.ToString();
+        text.text = hh.ToString("00")+ ":" + mm.ToString("00");//nos quitamos los molestos decimales de la hora
+
+
 
         float v = nightTimeCurve.Evaluate(Hours);//variable que se usa para el tiempo en horas, simplemente para tener una variable libre
         Color c = Color.Lerp(dayLightColor, nightLightColor, v);//Lerp sirve para cambiar el colo A y el color B en un intervalo de tiempo
