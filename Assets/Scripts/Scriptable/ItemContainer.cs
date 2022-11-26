feb9fb80c5f3cc5463ed0849828500443bb267da
GabrielKooseng <114022797+GabrielKooseng@users.noreply.github.com>
2022-11-25 22:35:52 -0800

    First half of the inventory system

---
+++ Assets/Scripts/Scriptable/ItemContainer.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+[System.Serializable]
+public class ItemSlot
+{
+    public Item item;
+    public int count;
+}
+
+[CreateAssetMenu(menuName ="Data/Item Container")]
+public class ItemContainer : ScriptableObject
+{
+    public List<ItemSlot> slots;
+}
